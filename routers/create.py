226b48a22d2c8bfe71aed3032d164881804a6f72
romamyronenko123 <eoma575@gmail.com>
2024-08-01 19:06:20 +0300

    update handlers

--- routers/create.py
+++ routers/create.py
@@ -1,5 +1,4 @@
-from aiogram import Router
-from aiogram.filters import Command
+from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
@@ -15,26 +14,28 @@ class Form(StatesGroup):
 router = Router()
 
 
-@router.message(Command("create"))
+@router.message(F.text.regexp(r"^/create$"))
 async def create(message: Message, state: FSMContext):
+    await message.answer('Введіть назву задачі: ')
+    await state.set_state(Form.title)
+
+
+@router.message(F.text.regexp(r"^/create \w+\.\s\w+$"))
+async def delete_one_line(message: Message, state: FSMContext):
     text = message.text
-    if text == '/create':
-        await message.answer('Введіть назву задачі: ')
-        await state.set_state(Form.title)
-    else:
-        title_description = text.split(" ", 1)[1]
-        title, description = title_description.split(". ", 1)
-        task = {'title': title, "description": description, "user_id": message.from_user.id}
-        db_manager.add_task(task)
-        await message.answer(f'title: {title}\ndescription: {description}')
+    title_description = text.split(" ", 1)[1]
+    title, description = title_description.split(". ", 1)
+    task = {'title': title, "description": description, "user_id": message.from_user.id}
+    db_manager.add_task(task)
+    await message.answer(f'title: {title}\ndescription: {description}')
 
 
 @router.message(Form.title)
 async def get_title(message: Message, state: FSMContext):
     title = message.text
-    await state.update_data(title=title)
+    await state.update_data(title=title)  # Цей ""state data зберігає наш title
     await state.set_state(Form.description)
-    await message.answer('Напишіть опис задачі!')
+    await message.answer('Напишіть опис задачі:')
 
 
 @router.message(Form.description)
@@ -46,14 +47,3 @@ async def get_description(message: Message, state: FSMContext):
     db_manager.add_task(task)
     await message.answer(f'title: {title}\ndescription: {description}')
     await state.clear()
-
-
-"""
-/create asd. ascxsad
-
-state: Form.description
-state.data: {'title': "EDFweaF"}
-
-description: "ASD32W DQ3EWFQ234FQ423TFQ234F"
-title: "EDFweaF"
-"""
--- routers/delete.py
+++ routers/delete.py
@@ -1,7 +1,4 @@
-import re
-
 from aiogram import Router, F
-from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, ReplyKeyboardRemove
